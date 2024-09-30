f3a67f949a04277d6970cedcf3a05c3fc9508b5e
milco <ujgold712@gmail.com>
2024-09-30 18:54:41 +0900

    기존파일 삭제

--- 01-starting-project/src/components/Tabs.jsx
+++
@@ -1,9 +0,0 @@
-export default function Tabs({ children, buttons, container }) {
-    const Container = container;
-    return <>
-        <Container>
-            {buttons}
-        </Container>
-        {children}
-    </>
-}
